34b9d01ef34823ec5b86ed1f1626989273a35f07
kdpone <pausangklyde@gmail.com>
2022-07-05 22:21:02 -0600

    joke is a string

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '74a7863eeemsh7769330fc331b88p1eab73jsnd9c279c69637',
+        'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+    }
+};
+let joke = ""
+
+fetch('https://dad-jokes.p.rapidapi.com/random/joke/png', options)
+    .then(response => response.json())
+    .then(response => {
+        joke = `${response.body.setup} ${response.body.punchline}`
+        return joke
+    })
+    .catch(err => console.error(err));
+
+
+
+
+
+
+
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-    console.log("Hello!!")
+    console.log("DadBot Online")
+    console.log(joke)
 })
+
+
+
+
+
 
 
 
